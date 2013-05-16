3e80cf3464c7180ce2e02d6ecbf531d160cf1969
Dimitry Solovyov <dimituri@gmail.com>
2013-05-16 19:59:36 +0300

    Temperature conversion multimethod

--- src/ldn_clojure/handler.clj
+++ src/ldn_clojure/handler.clj
@@ -8,9 +8,19 @@
   (let [join #(s/join " " %)]
     (some-> words (s/split #"\s+") reverse join)))
 
+(defmulti handle-temp (fn [from to _] [from to]))
+(defmethod handle-temp ["c" "f"] [_ _ value]
+  (some-> (-> value (* 9/5) (+ 32)) str))
+(defmethod handle-temp ["f" "c"] [_ _ value]
+  (some-> (-> value (* 5/9) (- 32)) str))
+(defmethod handle-temp :default [_ _ _] nil)
+
 (defroutes app-routes
   (GET "/" [] "CLOJURE WEB SERVICE 9000")
-  (GET "/reverse" {params :query-params} (handle-reverse params))
+  (GET "/reverse" {params :query-params}
+       (handle-reverse params))
+  (GET ["/temp/:from/:to/:value" :value #"-?(?:\d+.)?\d+"] [from to value]
+       (handle-temp from to (-> value read-string double)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
