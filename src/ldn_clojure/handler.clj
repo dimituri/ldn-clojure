95a5077cf2e0c0286a1489631e7e7061e8a0334d
Dimitry Solovyov <dimituri@gmail.com>
2013-05-16 18:29:28 +0300

    Reverse method

--- src/ldn_clojure/handler.clj
+++ src/ldn_clojure/handler.clj
@@ -1,10 +1,16 @@
 (ns ldn-clojure.handler
   (:use compojure.core)
-  (:require [compojure.handler :as handler]
+  (:require [clojure.string :as s]
+            [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(defn handle-reverse [{words "words"}]
+  (let [join #(s/join " " %)]
+    (some-> words (s/split #"\s+") reverse join)))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "CLOJURE WEB SERVICE 9000")
+  (GET "/reverse" {params :query-params} (handle-reverse params))
   (route/resources "/")
   (route/not-found "Not Found"))
 
