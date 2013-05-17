052f14b39e8ec05cb44f6d802c5f23581a158dda
Dimitry Solovyov <dimituri@gmail.com>
2013-05-17 13:13:50 +0300

    Very serious add method

--- src/ldn_clojure/handler.clj
+++ src/ldn_clojure/handler.clj
@@ -19,6 +19,9 @@
 (every 5000 #(println @counters) scheduler-pool)
 
 
+(defn handle-add [a b]
+  (str (+ a b)))
+
 (defn handle-reverse [{words "words"}]
   (let [join #(s/join " " %)]
     (some-> words (s/split #"\s+") reverse join)))
@@ -43,14 +46,18 @@
 (decorate handle-temp (with-register "temp"))
 
 
-(defroutes app-routes
-  (GET "/" [] "CLOJURE WEB SERVICE 9000")
-  (GET "/reverse" {params :query-params}
-       (handle-reverse params))
-  (GET ["/temp/:from/:to/:value" :value #"-?(?:\d+.)?\d+"] [from to value]
-       (handle-temp from to (-> value read-string double)))
-  (route/resources "/")
-  (route/not-found "Not Found"))
+(let [number #"-?(?:\d+.)?\d+"
+      string->double #(-> % read-string double)]
+  (defroutes app-routes
+    (GET "/" [] "CLOJURE WEB SERVICE 9000")
+    (GET ["/add/:a/:b" :a number :b number] [a b]
+         (handle-add (string->double a) (string->double b)))
+    (GET "/reverse" {params :query-params}
+         (handle-reverse params))
+    (GET ["/temp/:from/:to/:value" :value number] [from to value]
+         (handle-temp from to (string->double number)))
+    (route/resources "/")
+    (route/not-found "Not Found")))
 
 (def app
   (handler/site app-routes))
